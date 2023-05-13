86c516bf4a9a35e124dbc29434e7c9c3625fc885
chaturap <catur.ap@gmail.com>
2023-05-13 11:39:46 +0700

    working project, need testing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+import math
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -8,9 +8,11 @@ import pandas
 
 #url = "https://www.bcf.com.au/camping/tents/family-tents"
 # https://www.bcf.com.au/camping?prefn1=category&prefv1=Camping&start=0&sz=60
-key =  input('Please Input Category Camping : ')
-url = "https://www.bcf.com.au/camping?prefn1=category&prefv1={}&start=0&sz=60".format(key)
+#key1 =  input('Please Input Category Camping : ')
+#key2 =  input('Please Input Category Camping : ')
+#url = "https://www.bcf.com.au/{}?prefn1=category&prefv1={}&start=0&sz=60".format(key1,key2)
 #url = "https://www.bcf.com.au/camping/"
+url = ""
 params = {
     'prefn1': 'category',
     #'prefv1': 'Camping',
@@ -27,14 +29,17 @@ headers = {
 
 result = []
 def get_list(url: str):
-    url1 = "https://www.bcf.com.au/camping?prefn1=category&prefv1=Camping&start=0&sz=60"
-    url2 = "https://www.bcf.com.au/camping?prefn1=category&prefv1=Camping&start=0&sz=60"
-    url = "https://www.bcf.com.au/camping?prefn1=category&prefv1=Camping&start=0&sz=60"
+    #key = input('Please Input Category Camping/fhising : ')
+    #url = "https://www.bcf.com.au/camping?prefn1=category&prefv1={}&start=0&sz=60".format(key)
+    key = input('Please Input Category Camping : ')
+    key1 = key.lower()
+    key2 = key.title()
+    url = "https://www.bcf.com.au/{}?prefn1=category&prefv1={}&start=0&sz=60".format(key1, key2)
 
     res = requests.get(url, params=params,  headers=headers)
     if res.status_code == 200:
         print(f"Status Code is Ok , status code is : {res.status_code}")
-        soup = BeautifulSoup(res.text, 'html.parser')
+        #soup = BeautifulSoup(res.text, 'html.parser')
         # print(soup)
 
         try:
@@ -45,25 +50,59 @@ def get_list(url: str):
         for i in result:
             print(i)
         # proses scraping
-        contents = soup.find_all('li', {'class': 'grid-tile'})
+        # contents = soup.find_all('li', {'class': 'grid-tile'})
 
         count_page = 0
-        for page in (0,60,120):
-            url = f"https://www.bcf.com.au/camping?prefn1=category&prefv1=Camping&start={page}&sz=60"
-            print("url : ",url)
-            res = requests.get(url, params=params, headers=headers)
+        page : int = 0
+
+
+        res = requests.get(url, params=params, headers=headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        contents_paging = soup.find('div', {'class': 'results-hits'}).text.strip()
+        print(f"contents_paging: {contents_paging}")
+        print("cek showing", str.find(contents_paging, "Showing", 7))
+        print("cek total page : ", str.find(contents_paging, "-"))
+        awal = str.find(contents_paging, "-") + 1
+        akhir = str.find(contents_paging, "of")
+        awal_totalitem = str.find(contents_paging, "of") + 2
+        akhir_totalitem = str.find(contents_paging, " Results")
+        #print(f"awal:{awal} , akhir:{akhir}")
+        #print(f"awal:{awal_totalitem} , akhir:{akhir_totalitem}")
+
+        print("cek position of : ", str.find(contents_paging, "of"))
+        total_page = contents_paging[awal:akhir].strip()
+        print("total Page:", total_page)
+        total_item = contents_paging[awal_totalitem:akhir_totalitem].strip().replace(",", "")
+        print("total Item:", total_item)
+
+        last_page = math.ceil(int(total_item) / 60) * 60
+        print(f"last_page : {last_page}")
+
+        # 0 60 120 180 240    ...  3600
+        #3600 last_page
+        i = 1
+        while page < 180:
+
+            #res = requests.get(url, params=params, headers=headers)
+            #soup = BeautifulSoup(res.text, 'html.parser')
+            contents = soup.find_all('li', {'class': 'grid-tile'})
 
             for content in contents:
                 brand = content.find('div', attrs={'class': 'brand-name'}).text.strip()
                 name = content.find('div', attrs={'class': 'product-name'}).text.strip()
-                salesPrices = content.find('span', attrs={'class': 'product-sales-price'}).text.lstrip().rstrip().replace(" ","").replace("\r\n^","")
-                linkDetail = url + content.find('a', attrs={'class': 'thumb-link'})['href']
+                salesPrices = content.find('span', attrs={'class': 'product-sales-price'}).text.lstrip().rstrip().replace(" ","").replace("\r\n^","").replace("^","")
+                linkDetail = content.find('a', attrs={'class': 'thumb-link'})['href']
+                if "http" in linkDetail:
+                    linkDetail = content.find('a', attrs={'class': 'thumb-link'})['href']
+                else:
+                    linkDetail = url + content.find('a', attrs={'class': 'thumb-link'})['href']
+
                 sku = content.find('a', attrs={'class': 'thumb-link'})['href']
 
-                print(f"name : {name}")
-                print(f"salesPrices : {salesPrices}")
-                print(f"brand : {brand}")
-                print(f"linkDetail : {linkDetail}")
+                # print(f"name : {name}")
+                # print(f"salesPrices : {salesPrices}")
+                # print(f"brand : {brand}")
+                # print(f"linkDetail : {linkDetail}")
 
                 data_dict = {
                     'name' : name,
@@ -72,16 +111,19 @@ def get_list(url: str):
                     'linkDetail' : linkDetail,
                 }
 
-                print(data_dict)
+                #print(data_dict)
                 result.append(data_dict)
-                print(f"result : {result}")
+                #print(f"result : {result}")
                 try:
                     os.mkdir('json_result')
                 except FileExistsError:
                     pass
                 with open('json_result/final_data.json', 'w+') as json_data:
                     json.dump(result, json_data)
-            print('Json Created')
+            #print(f'Json Created page : {page}')
+            print(f"Scraping page ke :{i}")
+            i = i + 1
+            page = page+60
 
         # create csv file
         df = pd.DataFrame(result)
@@ -101,8 +143,6 @@ def get_total_pages():
         'prefv1': 'Camping',
         'src': '201',
         'sz': '60',
-        # 'keyword': searches,
-        # 'pgn': 1,
     }
 
     headers = {
@@ -133,25 +173,23 @@ def get_total_pages():
         total_item = contents[awal_totalitem:akhir_totalitem].strip().replace(",","")
         print("total Item:", total_item)
 
-        # for content in contents:
-        # total_pages = contents.find('div', attrs={'class': 'results-hits'}).text
-        #     total_item = contents.find('div', attrs={'div': 'results-hits'}).text
+        last_page = math.ceil(int(total_item) / 60) * 60
+        print(f"last_page : {last_page}")
 
 
-            #contents = contents.find('a')
-        #print(contents)
-            #print(total_pages)
-            #print(total_item)
+        print("i=")
+
+    return
 
 
-        # for content in contents:
-        #     brand = content.find('div', attrs={'class': 'pagination'}).text.strip()
-
-
-def get_item_per_pages():
-    pass
 
 if __name__ == '__main__':
     pass
     get_list(url)
     #get_total_pages()
+
+#web
+#https://www.bcf.com.au/p/tahwalhi-inflatable-stand-up-paddle-board-106---pearl-beach/635123.html?cgid=BCF04#main=&prefn1=isSale&prefv1=Featured&start=1
+
+#excel
+#https://www.bcf.com.au/watersports?prefn1=category&prefv1=Watersports&start=0&sz=60/p/tahwalhi-inflatable-stand-up-paddle-board-106---pearl-beach/635123.html?cgid=BCF04
